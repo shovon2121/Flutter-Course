c9e17471e18c66f6316c464c02f881bc927e7eed
shovon2121 <shovon15-7671@diu.edu.bd>
2022-06-03 09:27:30 +0600

    data types done

---
+++ Assignment_1/dataTypes.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  int x = 10;
+  double y = 10.08;
+  String name = "Shovon";
+  bool assingnmentDone = true;
+
+  print("""The value of x is: $x
+           The Value of y is: $y
+           The Value of assignmentDone is: $assingnmentDone
+  """);
+}
