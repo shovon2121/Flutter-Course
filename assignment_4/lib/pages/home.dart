9ce576482a41df156ee392c1f1231f91f54edd5b
shovon2121 <shovon15-7671@diu.edu.bd>
2022-06-22 00:54:21 +0600

    working on slider

--- assignment_4/lib/pages/home.dart
+++ assignment_4/lib/pages/home.dart
@@ -4,7 +4,42 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Screen"),),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 20,
+            left: 20
+          ),
+          child: Column(
+            children: [
+              Text(
+                "Hello Shovon",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.w700
+                ),
+              ),
+              Text(
+                "Let's gets something?",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xff4f4c4c)
+                ),
+              ),
+              SizedBox(height: 30,),
+              Container(
+                height: 130,
+                width: 250,
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
